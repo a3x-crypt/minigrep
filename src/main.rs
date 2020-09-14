1ff04eb6c7dec367f72f8da243e80851771a41b1
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-09-14 13:59:24 +0700

    Read file using fs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     
@@ -7,4 +9,8 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Error at reading the file");
+
+    println!("With text:\n{}", contents)
 }
